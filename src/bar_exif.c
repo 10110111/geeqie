d0cc864a8050573cbe14ad87334b01a0d21e8d11
Laurent Monin <geeqie@norz.org>
2008-04-12 06:42:47 +0000

    Add Exif.Photo.ColorSpace tag in Preferences > Properties > Exif. Requested by Frédéric Mantegazza.

--- src/bar_exif.c
+++ src/bar_exif.c
@@ -64,6 +64,7 @@ ExifUI ExifUIList[]={
 	{ 0, 0, EXIF_UI_IFSET, "Exif.Photo.ExposureProgram"},
 	{ 0, 0, EXIF_UI_IFSET, "Exif.Photo.MeteringMode"},
 	{ 0, 0, EXIF_UI_IFSET, "Exif.Photo.LightSource"},
+	{ 0, 0, EXIF_UI_IFSET, "Exif.Photo.ColorSpace"},
 	{ 0, 0, EXIF_UI_IFSET, "fSubjectDistance"},
 	{ 0, 0, EXIF_UI_OFF,   "fResolution"},
 	{ 0, 0, EXIF_UI_IFSET, "Exif.Image.Orientation"},
