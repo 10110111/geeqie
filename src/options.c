161a3cf407b2af7ce02dcf15792dd9ca8c5c8c16
Laurent Monin <geeqie@norz.org>
2008-04-24 09:09:48 +0000

    Move options stuff to options.{c,h} and remove globals.c

--- src/options.c
+++ src/options.c
@@ -1,18 +1,16 @@
 /*
  * Geeqie
- * (C) 2004 John Ellis
  * Copyright (C) 2008 The Geeqie Team
  *
- * Author: John Ellis
+ * Authors: Vladimir Nadvornik, Laurent Monin
  *
  * This software is released under the GNU General Public License (GNU GPL).
  * Please read the included file COPYING for more information.
  * This software comes with no warranty of any kind, use at your own risk!
  */
 
-
 #include "main.h"
-
+#include "options.h"
 
 ConfOptions *init_options(ConfOptions *options)
 {
