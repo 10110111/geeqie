e1c39fa29264aea839610dfedca5e086b5128395
Laurent Monin <geeqie@norz.org>
2008-05-03 12:50:52 +0000

    Add function prototypes (yet unused).

--- src/view_file.c
+++ src/view_file.c
@@ -16,4 +16,44 @@
 #include "view_file_list.h"
 #include "view_file_icon.h"
 
+ViewFile *vf_new(const gchar *path);
+
+void vf_set_status_func(ViewFile *vf, void (*func)(ViewFile *vf, gpointer data), gpointer data);
+void vf_set_thumb_status_func(ViewFile *vf, void (*func)(ViewFile *vf, gdouble val, const gchar *text, gpointer data), gpointer data);
+
+void vf_set_layout(ViewFile *vf, LayoutWindow *layout);
+
+gint vf_set_path(ViewFile *vf, const gchar *path);
+gint vf_refresh(ViewFile *vf);
+
+void vf_thumb_set(ViewFile *vf, gint enable);
+void vf_marks_set(ViewFile *vf, gint enable);
+void vf_sort_set(ViewFile *vf, SortType type, gint ascend);
+
+FileData *vf_index_get_data(ViewFile *vf, gint row);
+gchar *vf_index_get_path(ViewFile *vf, gint row);
+gint vf_index_by_path(ViewFile *vf, const gchar *path);
+gint vf_index_by_fd(ViewFile *vf, FileData *in_fd);
+gint vf_count(ViewFile *vf, gint64 *bytes);
+GList *vf_get_list(ViewFile *vf);
+
+gint vf_index_is_selected(ViewFile *vf, gint row);
+gint vf_selection_count(ViewFile *vf, gint64 *bytes);
+GList *vf_selection_get_list(ViewFile *vf);
+GList *vf_selection_get_list_by_index(ViewFile *vf);
+
+void vf_select_all(ViewFile *vf);
+void vf_select_none(ViewFile *vf);
+void vf_select_by_path(ViewFile *vf, const gchar *path);
+void vf_select_by_fd(ViewFile *vf, FileData *fd);
+
+void vf_mark_to_selection(ViewFile *vf, gint mark, MarkToSelectionMode mode);
+void vf_selection_to_mark(ViewFile *vf, gint mark, SelectionToMarkMode mode);
+
+void vf_select_marked(ViewFile *vf, gint mark);
+void vf_mark_selected(ViewFile *vf, gint mark, gint value);
+
+gint vf_maint_renamed(ViewFile *vf, FileData *fd);
+gint vf_maint_removed(ViewFile *vf, FileData *fd, GList *ignore_list);
+gint vf_maint_moved(ViewFile *vf, FileData *fd, GList *ignore_list);
 
