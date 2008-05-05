512e5dddd1d85da2fe030e0a464d953d0733cd1e
Laurent Monin <geeqie@norz.org>
2008-05-05 20:49:40 +0000

    Modify regen_potfiles.sh to generate a patch instead of directly applying changes to POTFILES.in.

--- po/regen_potfiles.sh
+++ po/regen_potfiles.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-(cd .. ; grep -l 'N\?_[[:space:]]*(.*)' ./src/*.c) > POTFILES.in
+#generate a patch to update POTFILES.in
+#Use like this: ./regen_potfiles.sh | patch -p0
+(cd .. ; grep -l 'N\?_[[:space:]]*(.*)' ./src/*.c) > POTFILES.in.$$
+diff -u POTFILES.in POTFILES.in.$$
+rm -f POTFILES.in.$$
