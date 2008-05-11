1fa5d80bd4c211fd31199ba5b8eab8ec638957d1
Laurent Monin <geeqie@norz.org>
2008-05-11 09:37:46 +0000

    Change translations statistics formatting to match the style use in the README file.
    Here is the result for current trunk:
    
    Translations statistics
    Date: Sun, 11 May 2008 11:33:14 +0200
    
    Note: completion % in the chart below may not be quite correct
          when fuzzy translations exist but do not appear in the source.
          For exact results, run make update-po with up to date POTFILES.in.
          comp % = trans / (trans + fuzzy + untrans)
    
    Language      Comp(%)  Trans  Fuzzy  Untrans  Total
    fr             100.00    985      0        0    985
    be              90.56    892     25       68    985
    eu              68.53    675    150      160    985
    eo              68.22    672    153      160    985
    vi              68.02    670    156      159    985
    it              67.82    668    154      163    985
    pt_BR           67.82    668    154      163    985
    zh_TW           66.90    659    150      176    985
    sk              66.50    655    158      172    985
    fi              64.37    634    176      175    985
    nl              64.37    634    179      172    985
    sv              64.16    632    175      178    985
    bg              64.06    631    175      179    985
    ca              64.06    631    175      179    985
    cs              64.06    631    175      179    985
    de              64.06    631    175      179    985
    es              64.06    631    175      179    985
    ja              64.06    631    175      179    985
    ru              64.06    631    175      179    985
    pl              60.61    597    148      240    985
    ar              42.94    423    315      247    985
    ro              37.56    370    342      273    985
    hu              36.14    356    332      297    985
    id              19.70    194    408      383    985
    et              19.29    190    407      388    985
    uk              17.16    169    411      405    985
    nb              15.94    157    416      412    985
    zh_CN.GB2312    15.94    157    416      412    985
    th              15.13    149    413      423    985
    sl              14.72    145    419      421    985
    da              12.99    128    402      455    985
    ko              11.37    112     52      821    985
    tr               6.60     65    398      522    985

--- po/gen_translations_stats.sh
+++ po/gen_translations_stats.sh
@@ -2,20 +2,28 @@
 
 # This script prints translations statistics for .po files
 # existing in the current directory
+export LC_ALL=C
 
 echo "Translations statistics"
-echo "Date: `date`"
+echo "Date: "$(date -R)
 echo
 
+echo "Note: completion % in the chart below may not be quite correct"
+echo "      when fuzzy translations exist but do not appear in the source."
+echo "      For exact results, run make update-po with up to date POTFILES.in."
+echo "      comp % = trans / (trans + fuzzy + untrans)"
+echo
+
+(echo "Language  Comp(%) Trans Fuzzy Untrans Total"; \
 for i in *.po; do
 	msgfmt --statistics -o /dev/null $i 2>&1 \
 	| sed 's/^\([0-9]\+ \)[^0-9]*\([0-9]\+ \)\?[^0-9]*\([0-9]\+ \)\?[^0-9]*$/\1\2\3/g' \
 	| awk '{ \
 		tot = $1 + $2 + $3; \
 		if (tot != 0) \
-			printf "%8.0f %8s %6.02f%% (%3d/%3d untranslated)\n",\
-			($1*100/tot)*100, "'"$i"'", $1*100/tot, $2+$3, tot}' ;
-done | sort -b -k1,1nr -k2,2 | sed 's/^ *[0-9]*//'
-
+			printf "%8.0f|%s|%7.2f|%5d|%5d|%7d|%5d\n",\
+			($1*100/tot)*100, "'"${i%%.po}"'", $1*100/tot, tot-($2+$3), $2, $3, tot}' ;
+done | sort -t '|' -b -k1,1nr -k2,2 | sed 's/^ *[0-9]*//' | tr ' |' '| '
+) | column -t -c 80 | tr '|' ' '
 echo
 
