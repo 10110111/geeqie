c278582ae1a6dfa2c1b12ce3af4554b28db37d27
Laurent Monin <geeqie@norz.org>
2009-06-29 16:02:28 +0000

    Fix POTFILES.in and update regen_potfiles.sh to add .desktop.in files.

--- po/regen_potfiles.sh
+++ po/regen_potfiles.sh
@@ -2,6 +2,9 @@
 
 #generate a patch to update POTFILES.in
 #Use like this: ./regen_potfiles.sh | patch -p0
-(cd .. ; grep -l 'N\?_[[:space:]]*(.*)' ./src/*.c) > POTFILES.in.$$
-diff -u POTFILES.in POTFILES.in.$$
-rm -f POTFILES.in.$$
+TMP=POTFILES.in.$$
+((find ../src/ -type f -name '*.c' ; find ../ -type f -name '*.desktop.in') | while read f; do
+	(echo $f | sed 's#^../##')
+done) | sort > $TMP
+diff -u POTFILES.in $TMP
+rm -f $TMP
