778b816f64effac3f60c5c374fa1d1dea98e2fc2
Laurent Monin <zas@norz.org>
2012-08-30 15:53:28 +0200

    Remove commented out code.

--- src/cache_maint.h
+++ src/cache_maint.h
@@ -16,15 +16,7 @@
 
 
 void cache_maintain_home(gboolean metadata, gboolean clear, GtkWidget *parent);
-
-#if 0
-gint cache_maintain_home_dir(const gchar *dir, gint recursive, gint clear);
-gint cache_maintain_dir(const gchar *dir, gint recursive, gint clear);
-#endif
-
 void cache_notify_cb(FileData *fd, NotifyType type, gpointer data);
-
-
 void cache_manager_show(void);
 
 
