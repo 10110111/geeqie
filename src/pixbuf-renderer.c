f46058baf808dcd4b3239f2dad03663ad1c27d0c
Colin Clark <colin.clark@cclark.uk>
2017-10-17 17:43:49 +0100

    Fix #313, #521: zoom increment is not multiplicative
    https://github.com/BestImageViewer/geeqie/issues/313
    https://github.com/BestImageViewer/geeqie/issues/521

--- src/pixbuf-renderer.c
+++ src/pixbuf-renderer.c
@@ -1251,24 +1251,38 @@ static void pr_zoom_adjust_real(PixbufRenderer *pr, gdouble increment,
 
 	if (increment < 0.0)
 		{
-		if (zoom >= 1.0 && zoom + increment < 1.0)
+		if (zoom >= 1.0)
 			{
-			zoom = zoom + increment - 2.0;
+			if (zoom / -(increment - 1.0) < 1.0)
+				{
+				zoom = 1.0 / (zoom / (increment - 1.0));
+				}
+			else
+				{
+				zoom = zoom / -(increment - 1.0) ;
+				}
 			}
 		else
 			{
-			zoom = zoom + increment;
+			zoom = zoom * -(increment - 1.0);
 			}
 		}
 	else
 		{
-		if (zoom <= -1.0 && zoom + increment > -1.0)
+		if (zoom <= -1.0 )
 			{
-			zoom = zoom + increment + 2.0;
+			if (zoom / (increment + 1.0) > -1.0)
+				{
+				zoom = -(1.0 / (zoom / (increment + 1.0)));
+				}
+			else
+				{
+				zoom = zoom / (increment + 1.0) ;
+				}
 			}
 		else
 			{
-			zoom = zoom + increment;
+			zoom = zoom * (increment + 1.0);
 			}
 		}
 
