cbe2074d6b330b9077444448a788b1ab1f309dfe
Klaus Ethgen <Klaus@Ethgen.de>
2017-10-29 13:39:18 +0100

    Fixing changelog generation

--- gen_changelog.sh
+++ gen_changelog.sh
@@ -12,7 +12,7 @@
 [ ! -x "$(command -v git)" ] && exit 0
 
 tail -6614 ChangeLog > ChangeLog.$$.old && \
-LC_ALL=C git log --no-merges --no-notes --encoding=UTF-8 --no-follow 1b58572cf58e9d2d4a0305108395dab5c66d3a09..HEAD > ChangeLog.$$.new && \
+LC_ALL=C git log --no-merges --no-notes --encoding=UTF-8 --no-follow --use-mailmap 1b58572cf58e9d2d4a0305108395dab5c66d3a09..HEAD > ChangeLog.$$.new && \
 cat ChangeLog.$$.old >> ChangeLog.$$.new && \
 mv -f ChangeLog ChangeLog.bak && \
 mv -f ChangeLog.$$.new ChangeLog
@@ -20,11 +20,18 @@ mv -f ChangeLog.$$.new ChangeLog
 rm -f ChangeLog.$$.old
 
 
-echo "<textarea rows='6615' cols='100'" >ChangeLog.$$.old.html && \
-tail -6618 ChangeLog >> ChangeLog.$$.old.html && \
-echo "</textarea" >>ChangeLog.$$.old.html && \
-LC_ALL=C git log --no-merges --no-notes --encoding=UTF-8 --date=format:'%Y-%m-%d' --no-follow  --pretty=format:"<li><a href=\"http://github.com/BestImageViewer/Geeqie/commit/%H\">view commit </a></li> <para>Author: %an<br>Date: %ad<br><textarea rows=4 cols=100>%s %n%n%b</textarea><br><br></para>" > ChangeLog.$$.new.html && \
-cat ChangeLog.$$.old.html >> ChangeLog.$$.new.html
+echo "<textarea rows='6614' cols='100'>" >ChangeLog.$$.old.html && \
+tail -6613 ChangeLog >> ChangeLog.$$.old.html && \
+echo "</textarea>" >>ChangeLog.$$.old.html && \
+echo "<html>" > ChangeLog.$$.new.html && \
+echo "<body>" >> ChangeLog.$$.new.html && \
+echo "<ul>" >> ChangeLog.$$.new.html && \
+LC_ALL=C git log --no-merges --no-notes --encoding=UTF-8 --date=format:'%Y-%m-%d' --no-follow --use-mailmap --pretty=format:"<li><a href=\"http://geeqie.org/cgi-bin/gitweb.cgi?p=geeqie.git;a=commit;h=%H\">view commit </a></li><p>Author: %aN<br>Date: %ad<br><textarea rows=4 cols=100>%s %n%n%b</textarea><br><br></p>" 1b58572cf58e9d2d4a0305108395dab5c66d3a09..HEAD >> ChangeLog.$$.new.html && \
+echo "" >> ChangeLog.$$.new.html && \
+cat ChangeLog.$$.old.html >> ChangeLog.$$.new.html && \
+echo "</ul>" >> ChangeLog.$$.new.html && \
+echo "</body>" >> ChangeLog.$$.new.html && \
+echo "</html>" >> ChangeLog.$$.new.html
 [ -e ChangeLog.html ] && mv -f ChangeLog.html ChangeLog.html.bak
 mv -f ChangeLog.$$.new.html ChangeLog.html
 
