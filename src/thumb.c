10110111/geeqie

commit 9be75e3431104d6a3a0c80324a8e3ac60316be13

Addl #510: Rudimentary video support
https://github.com/BestImageViewer/geeqie/issues/510

Add video files extension to default file filter (disabled by default).
Use recorded format_class in fd in metadata.c
Do not try to generate/read thumb for files which are not images.

src/thumb.c
@@ -337,6 +337,11 @@ gboolean thumb_loader_start(ThumbLoader *tl, FileData *fd)
 
 	if (!tl->fd) tl->fd = file_data_ref(fd);
 
+	if (tl->fd->format_class != FORMAT_CLASS_IMAGE && tl->fd->format_class != FORMAT_CLASS_RAWIMAGE)
+		{
+		thumb_loader_set_fallback(tl);
+		return FALSE;
+		}
 
 	if (tl->cache_enable)
 		{

src/thumb_standard.c
@@ -668,7 +668,7 @@ gboolean thumb_loader_std_start(ThumbLoaderStd *tl, FileData *fd)
 
 
 	tl->fd = file_data_ref(fd);
-	if (!stat_utf8(fd->path, &st))
+	if (!stat_utf8(fd->path, &st) || (tl->fd->format_class != FORMAT_CLASS_IMAGE && tl->fd->format_class != FORMAT_CLASS_RAWIMAGE))
 		{
 		thumb_loader_std_set_fallback(tl);
 		return FALSE;

src/filefilter.c
@@ -277,6 +277,14 @@ void filter_add_defaults(void)
 	filter_add_if_missing("erf", "Epson raw format", ".erf", FORMAT_CLASS_RAWIMAGE, FALSE, TRUE, TRUE);
 	filter_add_if_missing("srw", "Samsung raw format", ".srw", FORMAT_CLASS_RAWIMAGE, FALSE, TRUE, TRUE);
 	filter_add_if_missing("rw2", "Panasonic raw format", ".rw2", FORMAT_CLASS_RAWIMAGE, FALSE, TRUE, TRUE);
+
+	/* video files */
+	filter_add_if_missing("mp4", "MP4 video file", ".mp4;.m4v;.3gp;.3g2", FORMAT_CLASS_VIDEO, FALSE, FALSE, FALSE);
+	filter_add_if_missing("3gp", "3GP video file", ".3gp;.3g2", FORMAT_CLASS_VIDEO, FALSE, FALSE, FALSE);
+	filter_add_if_missing("mov", "MOV video file", ".mov;.qt", FORMAT_CLASS_VIDEO, FALSE, FALSE, FALSE);
+	filter_add_if_missing("avi", "AVI video file", ".avi", FORMAT_CLASS_VIDEO, FALSE, FALSE, FALSE);
+	filter_add_if_missing("mpg", "MPG video file", ".mpg;.mpeg;.mts;.m2ts;", FORMAT_CLASS_VIDEO, FALSE, FALSE, FALSE);
+	filter_add_if_missing("mkv", "Matroska video file", ".mkv;", FORMAT_CLASS_VIDEO, FALSE, FALSE, FALSE);
 }
 
 GList *filter_to_list(const gchar *extensions)

src/metadata.c
@@ -395,7 +395,7 @@ gboolean metadata_write_list(FileData *fd, const gchar *key, const GList *values
 			FileData *sfd = work->data;
 			work = work->next;
 
-			if (filter_file_class(sfd->extension, FORMAT_CLASS_META)) continue;
+			if (sfd->format_class == FORMAT_CLASS_META) continue;
 
 			metadata_write_list(sfd, key, values);
 			}

commit d37941b2e80c7ec1d0df38533e4e127ebca7684b

Thumbnail all files if filtering is disabled.
If the user has disabled filtering by file extension, then we should
also disable that filtering when generating/loading thumbnails. Fixes
regression from 9be75e3431104d6a3a0c80324a8e3ac60316be13.

src/thumb.c
@@ -337,7 +337,7 @@ gboolean thumb_loader_start(ThumbLoader *tl, FileData *fd)
 
 	if (!tl->fd) tl->fd = file_data_ref(fd);
 
-	if (tl->fd->format_class != FORMAT_CLASS_IMAGE && tl->fd->format_class != FORMAT_CLASS_RAWIMAGE && tl->fd->format_class != FORMAT_CLASS_VIDEO)
+	if (tl->fd->format_class != FORMAT_CLASS_IMAGE && tl->fd->format_class != FORMAT_CLASS_RAWIMAGE && tl->fd->format_class != FORMAT_CLASS_VIDEO && !options->file_filter.disable)
 		{
 		thumb_loader_set_fallback(tl);
 		return FALSE;

src/thumb_standard.c
@@ -668,7 +668,7 @@ gboolean thumb_loader_std_start(ThumbLoaderStd *tl, FileData *fd)
 
 
 	tl->fd = file_data_ref(fd);
-	if (!stat_utf8(fd->path, &st) || (tl->fd->format_class != FORMAT_CLASS_IMAGE && tl->fd->format_class != FORMAT_CLASS_RAWIMAGE && tl->fd->format_class != FORMAT_CLASS_VIDEO))
+	if (!stat_utf8(fd->path, &st) || (tl->fd->format_class != FORMAT_CLASS_IMAGE && tl->fd->format_class != FORMAT_CLASS_RAWIMAGE && tl->fd->format_class != FORMAT_CLASS_VIDEO && !options->file_filter.disable))
 		{
 		thumb_loader_std_set_fallback(tl);
 		return FALSE;